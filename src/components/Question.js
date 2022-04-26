88cf1da6d6aebc6922531eeb03a1fec8c10bc171
Philip Levy <philipdlevy@gmail.com>
2022-04-26 16:43:23 -0600

    commit tree 46c5526b6dbca6938757a182ab6632166864af9d

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  let timer = null
+
+  useEffect(() => {
+    setTimer()
+    if (timeRemaining === 0) {
+      onAnswered(false)
+      setTimeRemaining(10)
+    } 
+    return () => clearTimeout(timer);
+ }, [timeRemaining]);
+
+ function setTimer() {
+  timer = setTimeout(() => {  
+    setTimeRemaining(timeRemaining - 1)
+   }, 1000)
+   console.log("running")
+ }
+
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
